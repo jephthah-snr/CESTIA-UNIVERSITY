e008e01f540164446709484fc225f18ab1683dda
jephthah-snr <60290652+jephthah-snr@users.noreply.github.com>
2024-02-01 23:59:40 +0100

    Update array.js

--- 200L/array.js
+++ 200L/array.js
@@ -1,5 +1,5 @@
 const stringArray = ['nigeria','togo','benin','south afican','ghana','usa','france','barzil','south sudan','jamaica']
-const numberArray = [2,34,4564,3,567,4546,56,46,68,7,5456,678,789,34,66,4,6
+const numberArray = [2,34,4564,3,567,4546,56,46,68,7,5456,678,789,34,66,4,6}
 
 
 //NB: for each alway returns undefined
